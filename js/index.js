bfee4d54d3fde4b4773daf240ece7a8c53e977ad
Tommy Johnsen <7oiden@gmail.com>
2022-11-01 10:41:22 +0100

    slider-improved

--- js/index.js
+++ js/index.js
@@ -1,31 +1,27 @@
 const slider = document.querySelector(".slider");
 const leftArrow = document.querySelector("#left");
 const rightArrow = document.querySelector("#right");
-const indicatorParents = document.querySelector(".slider-preview");
-
+const indicatorParents = document.querySelector(".indicator-container");
 
 let sectionIndex = 0;
 
-document
-  .querySelectorAll(".preview-image")
-  .forEach(function (indicator, index) {
-    indicator.addEventListener("click", function () {
-      sectionIndex = index;
-      document.querySelector(".selected").classList.remove("selected");
-      indicator.classList.add("selected");
-      slider.style.transform = "translate(" + sectionIndex * -33.3333 + "%)";
-    });
+document.querySelectorAll(".indicator").forEach(function (indicator, index) {
+  indicator.addEventListener("click", function () {
+    sectionIndex = index;
+    document.querySelector(".selected").classList.remove("selected");
+    indicator.classList.add("selected");
+    slider.style.transform = "translate(" + sectionIndex * -33.3333 + "%)";
   });
+});
 
 leftArrow.addEventListener("click", function () {
   sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
   document.querySelector(".selected").classList.remove("selected");
-  indicatorParents.children[sectionIndex].classList.add("selected")
+  indicatorParents.children[sectionIndex].classList.add("selected");
   slider.style.transform = "translate(" + sectionIndex * -33.3333 + "%)";
 });
 
 rightArrow.addEventListener("click", function () {
-    console.log("hiy");
   sectionIndex = sectionIndex < 2 ? sectionIndex + 1 : 2;
   document.querySelector(".selected").classList.remove("selected");
   indicatorParents.children[sectionIndex].classList.add("selected");
